99b2c29b620fceb5b0469ee84d9a8346b0c9d77c
Niklas Springer <me@niklaspringer.xyz>
2023-05-26 15:47:19 +0200

    styling

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -1,49 +1,37 @@
 # frozen_string_literal: true
 
 class AnimeController < ApplicationController
+  PER_PAGE = 10
+
   def index
+    page = params[:page].to_i
+    offset = page * PER_PAGE
+
     url = URI.parse('https://api.myanimelist.net/v2/users/@me/animelist')
     headers = {
       'Authorization' => "Bearer #{session[:access_token]}"
     }
 
-    per_page = 100
-    offset = 0
-    @anime_list_data = []
-    @anime_info_data = [] # Add this line to initialize @anime_info_data
+    params = {
+      'fields' => 'list_status',
+      'limit' => PER_PAGE,
+      'offset' => offset
+    }
 
-    begin
-      loop do
-        params = {
-          'fields' => 'list_status',
-          'limit' => per_page,
-          'offset' => offset
-        }
+    response = make_api_call(url, headers, params)
+    if response.code.to_i == 200
+      anime_list = JSON.parse(response.body)
 
-        response = make_api_call(url, headers, params)
-        if response.code.to_i == 200
-          anime_list = JSON.parse(response.body)
-          break if anime_list['data'].empty?
-
-          @anime_list_data.concat(anime_list['data'])
-          offset += per_page
-        else
-          error_message = JSON.parse(response.body)['error_description']
-          flash[:alert] = "Failed to fetch anime list: #{error_message}" # Use flash[:alert] instead of redirecting
-          break # Break the loop if an error occurs
-        end
-      end
-    rescue StandardError => e
-      flash[:alert] = "An error occurred: #{e.message}" # Use flash[:alert] instead of redirecting
+      @anime_info_data = anime_list['data'].map do |anime|
+        anime_id = anime['node']['id']
+        get_anime_info(anime_id)
+      end.compact
+    else
+      error_message = JSON.parse(response.body)['error_description']
+      flash[:alert] = "Failed to fetch anime list: #{error_message}"
     end
-
-    @anime_list_data.each do |anime|
-      anime_id = anime['node']['id']
-      anime_info = get_anime_info(anime_id)
-
-      # Add the detailed anime information to the array
-      @anime_info_data << anime_info if anime_info
-    end
+  rescue StandardError => e
+    flash[:alert] = "An error occurred: #{e.message}"
   end
 
   private
@@ -68,24 +56,26 @@ class AnimeController < ApplicationController
   end
 
   def get_anime_info(anime_id)
-    url = URI.parse("https://api.myanimelist.net/v2/anime/#{anime_id}?fields=id,title,main_picture,start_date,end_date,synopsis,mean,rank,media_type,status,genres,my_list_status,rating")
+    Rails.cache.fetch("anime_info_#{anime_id}", expires_in: 12.hours) do
+      url = URI.parse("https://api.myanimelist.net/v2/anime/#{anime_id}?fields=id,title,main_picture,start_date,end_date,synopsis,mean,rank,media_type,status,genres,my_list_status,rating")
 
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
 
-    request = Net::HTTP::Get.new(url)
+      request = Net::HTTP::Get.new(url)
 
-    # Add any necessary headers, such as authorization if required
-    request['Authorization'] = "Bearer #{session[:access_token]}"
+      # Add any necessary headers, such as authorization if required
+      request['Authorization'] = "Bearer #{session[:access_token]}"
 
-    response = http.request(request)
+      response = http.request(request)
 
-    if response.code.to_i == 200
-      JSON.parse(response.body)
-      # Process and use the anime data as needed
-    else
-      # Handle error case
-      nil
+      if response.code.to_i == 200
+        JSON.parse(response.body)
+        # Process and use the anime data as needed
+      else
+        # Handle error case
+        nil
+      end
     end
   end
 end
